833525af51dfb96e00a1e26ea72d3bd8ece34506
Bryan Pease <bcp015000@gmail.com>
2014-10-22 21:14:50 -0500

    Created initial attempt at a core application object

---
+++ Application.js
@@ -0,0 +1,76 @@
+/**
+ * @copyright
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Cosmic Dynamo LLC
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * @module core.Applicaiton
+ */
+define([
+    "dojo/_base/declare",
+    "dojo/when",
+    "dojo/promise/all"
+], function (declare, when, all) {
+    /**
+     * @class core.Applicaiton
+     */
+    return declare([], {
+        started: false,
+        constructor: function(config){
+            this.config = config;
+
+            //TODO: Attach standard app components
+        },
+        /**
+         * Starts the application
+         * @returns {Promise<*>}
+         */
+        start: function(){
+            var loadConfig = this._run("loadConfig");
+
+            var init = when(loadConfig, function(){
+                return this._run("init");
+            }.bind(this));
+
+            return this.started =  when(init, function(app){
+                return this._run("start", app);
+            }.bind(this));
+        },
+        /**
+         *
+         * @param {String} method
+         * @returns {Promise<*>}
+         * @protected
+         */
+        _run: function(method){
+            var config = this.config;
+            var names = Object.keys(config);
+            var defList = [];
+            names.forEach(function(name){
+                var fn =app[name][method];
+                if (fn){
+                    defList.push(fn(app, config));
+                }
+            });
+
+            return defList.length > 1?all(defList):defList[0];
+        }
+    });
+});
