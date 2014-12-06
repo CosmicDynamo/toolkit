4b44052fb51799dd6e95b8d418afb841aabaeaf6
Bryan Pease <bcp015000@gmail.com>
2014-12-06 11:36:35 -0600

    Got coverter up and running w/ Unit Tests
    Used Accept header style input for the destination format to make it easier to hand off data to the formatter for sending between client and server

---
+++ convert.js
@@ -0,0 +1,120 @@
+/**
+ * @copyright
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Cosmic Dynamo LLC
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * @module core/converter
+ */
+define([
+    "dojo/_base/lang",
+    "blocks/Cache",
+    "blocks/Container",
+    "blocks/require",
+    "./header/accept"
+], function (lang, Cache, Container, require, parseAccept) {
+    var parsers = new Cache({
+        load: function(){
+            return new Container();
+        }
+    });
+    /**
+     * Runs the registered serializer, if able, on the input data
+     * @param {String} fromType - mediaType of the input data
+     * @param {String} accept - data type of the output
+     * @param {*} data - Data to be parsed
+     * @return {Promise<*> | null} - data formatted as requested; or null if no parser available
+     */
+    function convert(fromType, accept, data) {
+        var parts = fromType.split(";");
+
+        var from = parsers.get(parts[0]);
+
+        var toType = parseAccept(from.keys(), accept);
+
+        var to = from.get(toType);
+        if (!toType || !to) {
+            return null;
+        }
+
+        return require([to], function (parse) {
+            var options = {};
+            if (parts[1]){
+                parts[1].split(",").map(function(params){
+                    return params.split("=");
+                }).forEach(function(param){
+                    options[param[0].trim()] = param[1].trim()
+                });
+            }
+
+            return parse(data, options);
+        });
+    }
+    /**
+     * Registers converters using the provided moduleIds
+     * @param {Object} config
+     * @param {String} [config.type1] - mediaType string
+     * @param {String} [config.type2] - mediaType string
+     * @param {String} [config.type1-type2] - Module Id when converting from type1 to type2
+     * @param {String} [config.type2-type1] - Module Id when converting from type2 to type1
+     */
+    convert.register = function (config) {
+        this._parseConfig(config, function(info){
+            parsers.get(info.from).set(info.to, info.use);
+        });
+    };
+    /**
+     * Removes a registered convert
+     * @param {Object} config
+     * @param {String} [config.type1] - mediaType string
+     * @param {String} [config.type2] - mediaType string
+     * @param {String} [config.type1-type2] - Module Id when converting from type1 to type2
+     * @param {String} [config.type2-type1] - Module Id when converting from type2 to type1
+     */
+    convert.deregister = function (config) {
+        this._parseConfig(config, function(info){
+            parsers.get(info.from).remove(info.to);
+        });
+    };
+    /**
+     * Loops through a config object running the input function
+     * @param {Object} config
+     * @param {String} [config.type1] - mediaType string
+     * @param {String} [config.type2] - mediaType string
+     * @param {String} [config.type1-type2] - Module Id when converting from type1 to type2
+     * @param {String} [config.type2-type1] - Module Id when converting from type2 to type1
+     * @param {Function} fn - function to execute
+     * @private
+     */
+    convert._parseConfig = function(config, fn){
+        var keys = Object.keys(config);
+        keys.filter(function(key){return key.indexOf("-") > 0;})
+            .map(function(key){
+                var convert = key.split("-");
+                return {
+                    from: config[convert[0]],
+                    to: config[convert[1]],
+                    use: config[key]
+                }
+            }).forEach(fn);
+    };
+
+    return convert;
+});
