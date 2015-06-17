45c44c64cfefd44dd289a319975ac1d54372d778
Tollere <Tollere@users.noreply.github.com>
2015-06-17 13:49:24 -0500

    Updated blocks/parser/range.js to Intern.chai

--- tests/unit/blocks/parser/range.js
+++ tests/unit/blocks/parser/range.js
@@ -24,173 +24,150 @@
  * @module blocks.test.unit.parser.range
  */
 define([
-    "qasht/package/Unit",
+    "intern!object",
+    "intern/chai!assert",
     "blocks/parser/range",
     "blocks/parser/Data"
-], function (TestPackage, range, Data) {
-    return new TestPackage({
-        module: "blocks/parser/range",
-        tests: [
-            {
-                name: "Returns Empty Array at end of input",
-                input: "",
-                exec: function (test) {
-                    var out = range(test.data, 0, -1, function () {
-                        test.assertFail("End of string: can't get here");
-                    });
-
-                    test.assertIsArray(out);
-                    test.assertEqual(0, out.length);
-
-                    test.complete();
-                }
-            },
-            {
-                name: "Returns an array containing results from input function",
-                input: "The quick brown fox jumps over the lazy dog",
-                exec: function (test) {
-                    var out = range(test.data, 0, -1, function (data) {
-                        return data.next();
-                    });
-
-                    test.assertIsArray(out);
-                    test.assertEqual(test.input, out.join(""));
-
-                    test.complete();
-                }
-            },
-            {
-                name: "separator:Will look for separator between each function return",
-                input: "T,h,e, ,q,u,i,c,k, ,b,r,o,w,n, ,f,o,x, ,j,u,m,p,s, ,o,v,e,r, ,t,h,e, ,l,a,z,y, ,d,o,g",
-                exec: function (test) {
-                    var out = range(test.data, 0, -1, function (data) {
-                        return data.next();
-                    }, ",");
-
-                    test.assertIsArray(out);
-                    test.assertEqual(test.input.split(",").join(""), out.join(""));
-
-                    test.complete();
-                }
-            },
-            {
-                name: "separators: Will stop when no more separators are found",
-                input: "T,h,e quick brown fox jumps over the lazy dog",
-                exec: function (test) {
-                    var out = range(test.data, 0, -1, function (data) {
-                        return data.next();
-                    }, ",");
-
-                    test.assertIsArray(out);
-                    test.assertEqual("The", out.join(""));
-
-                    test.complete();
-                }
-            },
-            {
-                name: "separator: MAY be optional between each function return",
-                input: "T,h,e quick brown fox jumps over the lazy dog",
-                exec: function (test) {
-                    var out = range(test.data, 0, -1, function (data) {
-                        return data.next();
-                    }, ",", true);
-
-                    test.assertIsArray(out);
-                    test.assertEqual("The quick brown fox jumps over the lazy dog", out.join(""));
-
-                    test.complete();
-                }
-            },
-            {
-                name: "separator: Will ignore leading whitespace",
-                input: "T ,h ,e quick brown fox jumps over the lazy dog",
-                setUp: function (test) {
-                    test.data.whiteSpace = function (data) {
-                        data.pos++;
-                    }
-                },
-                exec: function (test) {
-                    var out = range(test.data, 0, -1, function (data) {
-                        return data.next();
-                    }, ",");
-
-                    test.assertIsArray(out);
-                    test.assertEqual("The", out.join(""));
-
-                    test.complete();
-                }
-            },
-            {
-                name: "separator: Is Case sensitive",
-                input: "TAhAea quick brown fox jumps over the lazy dog",
-                exec: function (test) {
-                    var out = range(test.data, 0, -1, function (data) {
-                        return data.next();
-                    }, "A");
-
-                    test.assertIsArray(out);
-                    test.assertEqual("The", out.join(""));
-
-                    test.data.pos = 0;
-                    out = range(test.data, 0, -1, function (data) {
-                        return data.next();
-                    }, "a");
-
-                    test.assertIsArray(out);
-                    test.assertEqual("T", out.join(""));
-
-                    test.complete();
-                }
-            },
-            {
-                name: "Will stop when end of input is reached",
-                input: "The quick brown fox jumps over the lazy dog",
-                exec: function (test) {
-                    var out = range(test.data, 0, -1, function (data) {
-                        return data.next();
-                    });
-
-                    test.assertIsArray(out);
-                    test.assertEqual(test.input, out.join(""));
-
-                    test.complete();
-                }
-            },
-            {
-                name: "Will return null if < min values were returned",
-                input: "T,h,e, ,q,u,ick brown fox jumps over the lazy dog",
-                exec: function (test) {
-                    var out = range(test.data, 10, -1, function (data) {
-                        return data.next();
-                    }, ",");
-
-                    test.assertNull(out);
-                    test.assertEqual(0, test.data.pos);
-
-                    test.complete();
-                }
-            },
-            {
-                name: "Will return null if > max values were returned",
-                input: "T,h,e, ,q,u,ick brown fox jumps over the lazy dog",
-                exec: function (test) {
-                    var out = range(test.data, 0, 2, function (data) {
-                        return data.next();
-                    }, ",");
-
-                    test.assertIsArray(out);
-                    test.assertEqual(2, out.length);
-                    test.assertEqual("Th", out.join(""));
-                    test.assertEqual(4, test.data.pos);
-
-                    test.complete();
-                }
-            }
-        ],
-        setUp: function (test) {
-            test.data = new Data({
-                input: test.input
+], function (TestSuite, assert, range, Data) {
+    return TestSuite({
+        name: "blocks/parser/range",
+        
+        "Returns Empty Array at end of input": function () {
+            var data = new Data({
+                input: ""
             });
+            var out = range(data, 0, -1, function () {
+                //this line is designed to always throw a failure if reached
+                assert.fail(data,true,"End of string: can't get here");
+            });
+
+            assert.isArray(out);
+            assert.strictEqual(0, out.length);
+        },
+
+        "Returns an array containing results from input function": function () {
+            var data = new Data({
+                input: "The quick brown fox jumps over the lazy dog"
+            });
+            var out = range(data, 0, -1, function (data) {
+                return data.next();
+            });
+            assert.isArray(out);
+            assert.strictEqual(test.input, out.join(""));
+        },
+
+        "separator:Will look for separator between each function return": function () {
+            var data = new Data({
+                input: "T,h,e, ,q,u,i,c,k, ,b,r,o,w,n, ,f,o,x, ,j,u,m,p,s, ,o,v,e,r, ,t,h,e, ,l,a,z,y, ,d,o,g"
+            });
+            var out = range(data, 0, -1, function (data) {
+                return data.next();
+            }, ",");
+
+            assert.isArray(out);
+            assert.strictEqual(test.input.split(",").join(""), out.join(""));
+        },
+        
+        "separators: Will stop when no more separators are found": function () {
+            var data = new Data({
+                input: "T,h,e quick brown fox jumps over the lazy dog"
+            });
+            var out = range(data, 0, -1, function (data) {
+                return data.next();
+            }, ",");
+
+            assert.isArray(out);
+            assert.strictEqual("The", out.join(""));
+        
+        },
+        
+        "separator: MAY be optional between each function return": function () {
+            var data = new Data({
+                input: "T,h,e quick brown fox jumps over the lazy dog"
+            });
+            var out = range(data, 0, -1, function (data) {
+                return data.next();
+            }, ",", true);
+
+            assert.isArray(out);
+            assert.strictEqual("The quick brown fox jumps over the lazy dog", out.join(""));
+        },
+        
+        
+        "separator: Will ignore leading whitespace": function () {
+            var data = new Data({
+                input: "T ,h ,e quick brown fox jumps over the lazy dog"
+            });
+
+            data.whiteSpace = function (data) {
+                data.pos++;
+            };
+
+            
+            var out = range(data, 0, -1, function (data) {
+                return data.next();
+            }, ",");
+
+            assert.isArray(out);
+            assert.strictEqual("The", out.join(""));
+        },
+            
+        "separator: Is Case sensitive": function () {
+            var data = new Data({
+                input: "TAhAea quick brown fox jumps over the lazy dog"
+            });
+            var out = range(data, 0, -1, function (data) {
+                return data.next();
+            }, "A");
+
+            assert.isArray(out);
+            assert.strictEqual("The", out.join(""));
+
+            data.pos = 0;
+            out = range(data, 0, -1, function (data) {
+                return data.next();
+            }, "a");
+
+            assert.isArray(out);
+            assert.strictEqual("T", out.join(""));
+        },
+
+        "Will stop when end of input is reached": function () {
+            var data = new Data({
+                input: "The quick brown fox jumps over the lazy dog"
+            });
+            var out = range(data, 0, -1, function (data) {
+                return data.next();
+            });
+
+            assert.isArray(out);
+            assert.strictEqual("The quick brown fox jumps over the lazy dog", out.join(""));
+        },
+        
+        "Will return null if < min values were returned": function () {
+            var data = new Data({
+                input: "T,h,e, ,q,u,ick brown fox jumps over the lazy dog"
+            });
+            var out = range(data, 10, -1, function (data) {
+                return data.next();
+            }, ",");
+
+            assert.isNull(out);
+            assert.strictEqual(0, data.pos);
+        },
+        
+        "Will return null if > max values were returned": function () {
+            var data = new Data({
+                input: "T,h,e, ,q,u,ick brown fox jumps over the lazy dog"
+            });
+            var out = range(data, 0, 2, function (data) {
+                return data.next();
+            }, ",");
+
+            assert.isArray(out);
+            assert.strictEqual(2, out.length);
+            assert.strictEqual("Th", out.join(""));
+            assert.strictEqual(4, data.pos);
         }
     });
 });
