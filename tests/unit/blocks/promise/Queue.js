5217278f86539c19f71c9c12d9fe6946f9f14052
jsecor <jrsecor@yahoo.com>
2015-07-03 10:58:12 -0500

    Queue.js Test Conversion

--- tests/unit/blocks/promise/Queue.js
+++ tests/unit/blocks/promise/Queue.js
@@ -24,277 +24,226 @@
  * @module blocks.test.unit.promise.Queue
  */
 define([
-    "qasht/package/Unit",
+    "intern!object",
+    "intern/chai!assert",
     "blocks/promise/Queue",
     "dojo/_base/Deferred"
-], function (TestPackage, Queue, Deferred) {
-    return new TestPackage({
-        module: "blocks/promise/Queue",
-        tests: [
-            {
-                name: "enqueue: executes first queued fn sync",
-                exec: function(test){
-                    var scope = {};
-                    var queue = new Queue(scope);
+], function (TestSuite, assert, Queue, Deferred) {
+    return new TestSuite({
+        name: "blocks/promise/Queue",
+        "enqueue: executes first queued fn sync": function(){
+            var scope = {};
+            var queue = new Queue(scope);
 
-                    queue.enqueue(function(){
-                        this.value = true;
-                    });
+            queue.enqueue(function(){
+                this.value = true;
+            });
 
-                    test.assertTrue(scope.value, "Context was updated");
-
-                    test.complete();
-                }
-            },
-            {
-                name: "enqueue: function execution context matches constructor param",
-                exec: function (test) {
-                    var scope = {};
-                    var queue = new Queue(scope);
-
-                    queue.enqueue(function(){
-                        this.value = true;
-                    });
-
-                    test.assertTrue(scope.value, "Context was updated");
-
-                    test.complete();
-                }
-            },
-            {
-                name: "enqueue: executes each function in the order it was queue: no promise",
-                exec: function (test) {
-                    var scope = {order:0};
-                    var queue = new Queue(scope);
-
-                    queue.enqueue(function(){
-                        test.assertEqual(0, this.order++, "1st function");
-                    });
-
-                    queue.enqueue(function(){
-                        test.assertEqual(1, this.order++, "2nd function");
-                    });
-
-                    test.assertEqual(2, scope.order);
-
-                    test.complete();
-                }
-            },
-            {
-                name: "enqueue: executes each function in the order it was queue: promise",
-                exec: function (test) {
-                    var scope = {
-                        order:0,
-                        "0":new Deferred(),
-                        "1":new Deferred()
-                    };
-                    var queue = new Queue(scope);
-
-                    queue.enqueue(function(){
-                        test.assertEqual(0, this.order++, "1st function");
-                        return this["0"];
-                    });
-
-                    queue.enqueue(function(){
-                        test.assertEqual(1, this.order++, "2nd function");
-                        return this["1"];
-                    });
-
-                    var last = queue.enqueue(function(){
-                        test.assertEqual(2, this.order++, "2nd function");
-                    });
-
-                    test.assertEqual(1, scope.order);
-                    scope["1"].resolve(1);
-                    test.assertEqual(1, scope.order);
-
-                    test.whenResolved(last, function(){
-                        test.assertEqual(3, scope.order);
-
-                        test.complete();
-                    });
-
-                    scope["0"].resolve();
-                }
-            },
-            {
-                name: "enqueue: if args not specified; last return passed into next function: no promise",
-                exec: function (test) {
-                    var scope = {order:0};
-                    var queue = new Queue(scope);
-
-                    queue.enqueue(function(){
-                        return 1
-                    });
-
-                    var rtn = queue.enqueue(function(last){
-                        return last + 1;
-                    });
-
-                    test.assertEqual(2, rtn);
-
-                    test.complete();
-                }
-            },
-            {
-                name: "enqueue: if args not specified; last return passed into next function: promise",
-                exec: function (test) {
-                    var scope = {
-                        "0":new Deferred(),
-                        "1":new Deferred()
-                    };
-                    var queue = new Queue(scope);
-
-                    queue.enqueue(function(){
-                        return this["0"];
-                    });
-
-                    queue.enqueue(function(val){
-                        test.assertEqual(1, val, "2nd function");
-                        return this["1"];
-                    });
-
-                    var last = queue.enqueue(function(val){
-                        test.assertEqual(2, val, "2nd function");
-                        return val + 1;
-                    });
-
-                    test.whenResolved(last, function(val){
-                        test.assertEqual(3, val);
-
-                        test.complete();
-                    });
-
-                    scope["1"].resolve(2);
-                    scope["0"].resolve(1);
-                }
-            },
-            {
-                name: "enqueue: arges are passed into enqueued fn: no promise",
-                exec: function (test) {
-                    var scope = {order:0};
-                    var queue = new Queue(scope);
-
-                    queue.enqueue(function(p1, p2){
-                        test.assertEqual("1", p1);
-                        test.assertEqual("2", p2);
-                        return "Error"
-                    }, ["1", "2"]);
-
-                    queue.enqueue(function(p1, p2){
-                        test.assertEqual("3", p1);
-                        test.assertEqual("4", p2);
-                    }, ["3", "4"]);
-
-                    test.complete();
-                }
-            },
-            {
-                name: "enqueue: args are passed into enqueued fn: promise",
-                exec: function (test) {
-                    var scope = {
-                        "0":new Deferred(),
-                        "1":new Deferred()
-                    };
-                    var queue = new Queue(scope);
-
-                    queue.enqueue(function(p1, p2){
-                        test.assertEqual("1", p1);
-                        test.assertEqual("2", p2);
-                        return this["0"];
-                    }, ["1", "2"]);
-
-                    queue.enqueue(function(p1, p2){
-                        test.assertEqual("3", p1);
-                        test.assertEqual("4", p2);
-                        return this["1"];
-                    }, ["3", "4"]);
-
-                    var last = queue.enqueue(function(p1, p2){
-                        test.assertEqual("5", p1);
-                        test.assertEqual("6", p2);
-                        return "done";
-                    }, ["5", "6"]);
-
-                    test.whenResolved(last, function(p1){
-                        test.assertEqual("done", p1);
-
-                        test.complete();
-                    });
-
-                    scope["1"].resolve("fail");
-                    scope["0"].resolve("fail");
-                }
-            },
-            {
-                name: "enqueue: queue can be stopped in the event of an error: return promise rejected",
-                exec: function (test) {
-                    var scope = {
-                        "0":new Deferred(),
-                        "1":new Deferred()
-                    };
-                    var queue = new Queue(scope);
-
-                    queue.enqueue(function(){
-                        return this["0"];
-                    });
-                    queue.enqueue(function(){
-                        return this["1"];
-                    });
-
-                    var last = queue.enqueue(function() {
-                        test.assertFail("This function should not be executed");
-                    });
-
-                    test.whenRejected(last, function(p1){
-                        test.assertEqual("pass", p1);
-
-                        test.complete();
-                    });
-
-                    scope["1"].reject("pass");
-                    scope["0"].resolve("fail");
-                }
-            },
-            {
-                name: "onError: constructor errBack fn will prevent queue stopped",
-                exec: function (test) {
-                    var scope = {
-                        "0": new Deferred(),
-                        "1": new Deferred()
-                    };
-                    var queue = new Queue(scope, function () {
-                        scope.err = true;
-                    });
-
-                    queue.enqueue(function () {
-                        return this["0"];
-                    });
-
-                    queue.enqueue(function () {
-                        return this["1"];
-                    });
-
-                    var last = queue.enqueue(function () {
-                        this.lastRun = true;
-                    });
-
-                    test.whenResolved(last, function () {
-                        test.assertTrue(scope.err, "Error fn was called");
-                        test.assertTrue(scope.lastRun, "Last fn was called");
-
-                        test.complete();
-                    });
-
-                    scope["1"].reject("pass");
-                    scope["0"].resolve("fail");
-
-                }
-            }
-        ],
-        setUp: function (test) {
+            assert.isTrue(scope.value, "Context was updated");
         },
-        tearDown: function (test) {
+        "enqueue: function execution context matches constructor param": function () {
+            var scope = {};
+            var queue = new Queue(scope);
+
+            queue.enqueue(function(){
+                this.value = true;
+            });
+
+            assert.isTrue(scope.value, "Context was updated");
+        },
+        "enqueue: executes each function in the order it was queue: no promise": function () {
+            var scope = {order:0};
+            var queue = new Queue(scope);
+
+            queue.enqueue(function(){
+                assert.strictEqual(0, this.order++, "1st function");
+            });
+
+            queue.enqueue(function(){
+                assert.strictEqual(1, this.order++, "2nd function");
+            });
+
+            assert.strictEqual(2, scope.order);
+        },
+        "enqueue: executes each function in the order it was queue: promise": function () {
+            var scope = {
+                order:0,
+                "0":new Deferred(),
+                "1":new Deferred()
+            };
+            var queue = new Queue(scope);
+
+            queue.enqueue(function(){
+                assert.strictEqual(0, this.order++, "1st function");
+                return this["0"];
+            });
+
+            queue.enqueue(function(){
+                assert.strictEqual(1, this.order++, "2nd function");
+                return this["1"];
+            });
+
+            var last = queue.enqueue(function(){
+                assert.strictEqual(2, this.order++, "2nd function");
+            });
+
+            assert.strictEqual(1, scope.order);
+            scope["1"].resolve(1);
+            assert.strictEqual(1, scope.order);
+
+            var done = this.async();
+            when(last, done.callback(function(){
+                assert.strictEqual(3, scope.order);
+            }));
+
+            scope["0"].resolve();
+        },
+        "enqueue: if args not specified; last return passed into next function: no promise": function () {
+            var scope = {order:0};
+            var queue = new Queue(scope);
+
+            queue.enqueue(function(){
+                return 1
+            });
+
+            var rtn = queue.enqueue(function(last){
+                return last + 1;
+            });
+
+            assert.strictEqual(2, rtn);
+        },
+        "enqueue: if args not specified; last return passed into next function: promise": function () {
+            var scope = {
+                "0":new Deferred(),
+                "1":new Deferred()
+            };
+            var queue = new Queue(scope);
+
+            queue.enqueue(function(){
+                return this["0"];
+            });
+
+            queue.enqueue(function(val){
+                assert.strictEqual(1, val, "2nd function");
+                return this["1"];
+            });
+
+            var last = queue.enqueue(function(val){
+                assert.strictEqual(2, val, "2nd function");
+                return val + 1;
+            });
+
+            var done = this.async();
+            when(last, done.callback(function(val){
+                assert.strictEqual(3, val);
+            }));
+
+            scope["1"].resolve(2);
+            scope["0"].resolve(1);
+        },
+        "enqueue: arges are passed into enqueued fn: no promise": function () {
+            var scope = {order:0};
+            var queue = new Queue(scope);
+
+            queue.enqueue(function(p1, p2){
+                assert.strictEqual("1", p1);
+                assert.strictEqual("2", p2);
+                return "Error"
+            }, ["1", "2"]);
+
+            queue.enqueue(function(p1, p2){
+                assert.strictEqual("3", p1);
+                assert.strictEqual("4", p2);
+            }, ["3", "4"]);
+        },
+        "enqueue: args are passed into enqueued fn: promise": function () {
+            var scope = {
+                "0":new Deferred(),
+                "1":new Deferred()
+            };
+            var queue = new Queue(scope);
+
+            queue.enqueue(function(p1, p2){
+                assert.strictEqual("1", p1);
+                assert.strictEqual("2", p2);
+                return this["0"];
+            }, ["1", "2"]);
+
+            queue.enqueue(function(p1, p2){
+                assert.strictEqual("3", p1);
+                assert.strictEqual("4", p2);
+                return this["1"];
+            }, ["3", "4"]);
+
+            var last = queue.enqueue(function(p1, p2){
+                assert.strictEqual("5", p1);
+                assert.strictEqual("6", p2);
+                return "done";
+            }, ["5", "6"]);
+
+            var done = this.async();
+            when(last, done.callback(function(p1){
+                assert.strictEqual("done", p1);
+            }));
+
+            scope["1"].resolve("fail");
+            scope["0"].resolve("fail");
+        },
+        "enqueue: queue can be stopped in the event of an error: return promise rejected": function () {
+            var scope = {
+                "0":new Deferred(),
+                "1":new Deferred()
+            };
+            var queue = new Queue(scope);
+
+            queue.enqueue(function(){
+                return this["0"];
+            });
+            queue.enqueue(function(){
+                return this["1"];
+            });
+
+            var last = queue.enqueue(function() {
+                assert.ok(false, "This function should not be executed");
+            });
+
+            var done = this.async();
+            when(last, done.errback(function(p1){
+                assert.strictEqual("pass", p1);
+            }));
+
+            scope["1"].reject("pass");
+            scope["0"].resolve("fail");
+        },
+        "onError: constructor errBack fn will prevent queue stopped": function () {
+            var scope = {
+                "0": new Deferred(),
+                "1": new Deferred()
+            };
+            var queue = new Queue(scope, function () {
+                scope.err = true;
+            });
+
+            queue.enqueue(function () {
+                return this["0"];
+            });
+
+            queue.enqueue(function () {
+                return this["1"];
+            });
+
+            var last = queue.enqueue(function () {
+                this.lastRun = true;
+            });
+
+            var done = this.async();
+            when(last, done.progback(function () {
+                assert.isTrue(scope.err, "Error fn was called");
+                assert.isTrue(scope.lastRun, "Last fn was called");
+            }));
+
+            scope["1"].reject("pass");
+            scope["0"].resolve("fail");
         }
     });
 });
